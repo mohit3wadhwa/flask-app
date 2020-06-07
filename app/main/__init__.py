89bce751704dd48a6c2a780b5f52b959b959fb1d
Mohit Wadhwa <mohit3wadhwa@gmail.com>
2020-06-07 01:57:16 +0100

    Update __init__.py

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -2,11 +2,7 @@ from flask import Blueprint
 
 main = Blueprint('main', __name__)
 
-<<<<<<< HEAD
-from . import home, errors, signup, admin, login
-=======
 from . import home, login, errors, signup, tambola
->>>>>>> 92e10fb7a572491c3e52a147c6275b2fb194599c
 from ..models import Permission
 
 # from .apps import apps as apps_blueprint
